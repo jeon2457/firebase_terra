05728516baf3a13b3200070a61770b896af80688
jeon2457 <jeon2457@gmail.com>
2026-02-16 10:18:13 +0900

    upload

--- next-app/src/models/MapLocation.ts
+++ next-app/src/models/MapLocation.ts
@@ -1,26 +1,19 @@
-import mongoose, { Schema, Document, Model } from 'mongoose';
+import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IMapLocation extends Document {
-  _id: number;
-  addr: string;
-  road_address: string;
-  lat: number;
-  lng: number;
-  notice: string;
-  updated_at: Date;
+    addr: string;
+    lat: number;
+    lng: number;
+    notice: string;
 }
 
-const MapLocationSchema: Schema = new Schema({
-  _id: { type: Number, default: 1 }, // 항상 1개의 문서만 유지 (ID 고정)
-  addr: { type: String, required: true },
-  road_address: { type: String },
-  lat: { type: Number, required: true },
-  lng: { type: Number, required: true },
-  notice: { type: String },
-  updated_at: { type: Date, default: Date.now }
+const MapLocationSchema = new Schema<IMapLocation>({
+    addr: { type: String, required: true },
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    notice: { type: String, default: '' }
+}, { 
+    timestamps: true 
 });
 
-// 모델이 이미 존재하면 재사용, 없으면 생성
-const MapLocation: Model<IMapLocation> = mongoose.models.MapLocation || mongoose.model<IMapLocation>('MapLocation', MapLocationSchema);
-
-export default MapLocation;
+export default mongoose.models.MapLocation || mongoose.model<IMapLocation>('MapLocation', MapLocationSchema);
