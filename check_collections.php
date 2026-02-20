8b4e0ba70c731d01abca076ef211be44c325337a
jeon2457 <jeon2457@gmail.com>
2026-02-20 22:23:36 +0900

    delete

--- check_collections.php
+++
@@ -1,6 +0,0 @@
-<?php
-require './php/db-connect-mongo.php';
-$collections = $database->listCollections();
-foreach ($collections as $collectionInfo) {
-    echo $collectionInfo->getName() . "\n";
-}
--- list_collections.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/php/db-connect-mongo.php';
-$collections = $database->listCollections();
-foreach ($collections as $collection) {
-    echo $collection->getName() . PHP_EOL;
-}
